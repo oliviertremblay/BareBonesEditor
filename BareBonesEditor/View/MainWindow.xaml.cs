733c815eaba33dedc54685ecb7a051eda3968601
oliviertremblay <olivier.tremblay.10@gmail.com>
2025-04-30 15:02:03 -0400

    Add project files

---
+++ BareBonesEditor/View/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BareBonesEditor.ViewModel;
+using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.CodeCompletion;
+using ICSharpCode.AvalonEdit.Highlighting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace BareBonesEditor;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = new MainViewModel(editor);
+
+        Closing += (s, e) =>
+        {
+            if (DataContext is MainViewModel vm)
+                editor.Save(MainViewModel.FilePath);
+        };
+    }
+}
---
+++ BareBonesEditor/ViewModel/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using BareBonesEditor.Utils;
+using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.Highlighting;
+
+namespace BareBonesEditor.ViewModel
+{
+    class MainViewModel
+    {
+        public static string FilePath
+        {
+            get { return "exam.cs"; }
+        }
+
+        private readonly TextEditor _editor;
+
+        public MainViewModel(TextEditor editor)
+        {
+            _editor = editor;
+            SaveCommand = new RelayCommand(SaveFile);
+
+            editor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinition("C#");
+
+            if (File.Exists(FilePath))
+            {
+                editor.Load(FilePath);
+            }
+        }
+
+        public ICommand SaveCommand { get; set; }
+
+        private void SaveFile()
+        {
+            File.WriteAllText(FilePath, _editor.Text);
+        }
+    }
+}
